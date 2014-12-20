ef4457b5e2015f34df1bf4f829053d667147bda7
Edward A Maxedon Sr <edadma@gmail.com>
2014-12-19 23:05:30 -0500

    added .travis.yml

--- src/main/scala/TestMain.scala
+++ src/main/scala/TestMain.scala
@@ -4,9 +4,24 @@ package ca.hyperreal.__markdown__
 object TestMain extends App
 {
 	val s =
-"""`` asdf zx`cv ``
+"""
+5.  Can list markers be indented?  Can ordered list markers be right-aligned?
 
-asdf"""
+    ``` markdown
+     8. item 1
+     9. item 2
+    10. item 2a
+    ```
+
+6.  Is this one list with a horizontal rule in its second item,
+    or two lists separated by a horizontal rule?
+
+    ``` markdown
+    * a
+    * * * * *
+    * b
+    ```
+"""
 
 	println( Markdown(s) )
 //	println( headings )
