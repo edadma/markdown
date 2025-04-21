a175e99de13406d7dd33d7f929344ab9cdeefbce
Edward A Maxedon <edadma@gmail.com>
2025-04-21 16:03:04 -0400

    parseDocument

--- shared/src/main/scala/io/github/edadma/markdown/Node.scala
+++ shared/src/main/scala/io/github/edadma/markdown/Node.scala
@@ -3,12 +3,12 @@ package io.github.edadma.markdown
 sealed trait Node
 case class Document(children: List[Block]) extends Node
 
-sealed trait Block                                                       extends Node
-case class Paragraph(inlines: List[Inline])                              extends Block
-case class Heading(level: Int, inlines: List[Inline])                    extends Block
-case class CodeBlock(content: String, infoString: Option[String] = None) extends Block
-case class BlockQuote(children: List[Block])                             extends Block
-case class ThematicBreak()                                               extends Block
+sealed trait Block                                                  extends Node
+case class Paragraph(inlines: List[Inline])                         extends Block
+case class Heading(level: Int, inlines: List[Inline])               extends Block
+case class Code(content: String, infoString: Option[String] = None) extends Block
+case class BlockQuote(children: List[Block])                        extends Block
+case class ThematicBreak()                                          extends Block
 
 sealed trait Inline              extends Node
 case class Text(content: String) extends Inline
--- shared/src/main/scala/io/github/edadma/markdown/parseDocument.scala
+++ shared/src/main/scala/io/github/edadma/markdown/parseDocument.scala
@@ -1,64 +1,99 @@
-// File: parseDocument.scala
 package io.github.edadma.markdown
 
+// Add to Node.scala:
+// case class Heading(level: Int, inlines: List[Inline]) extends Block
+// case class CodeBlock(content: String, infoString: Option[String] = None) extends Block
+// case class BlockQuote(children: List[Block]) extends Block
+// case class ThematicBreak() extends Block
+
+// Block parser implementation
 def parseDocument(stream: LazyList[Cursor]): Document = {
   val blocks = parseBlocks(stream)
   Document(blocks)
 }
 
+// The main block parsing function that delegates to specific block parsers
 private def parseBlocks(stream: LazyList[Cursor]): List[Block] = {
-  // Group cursor stream into paragraphs while preserving cursors
-  val paragraphs = collectParagraphs(stream)
-
-  // Initially treat each paragraph as containing a single Text node
-  // Later, we'll replace this with real inline parsing
-  paragraphs.map { cursors =>
-    // For now, just concatenate the characters from cursors into a string
-    val text = cursors.map(_.char).mkString
-    Paragraph(List(Text(text)))
-  }
-}
-
-private def collectParagraphs(stream: LazyList[Cursor]): List[LazyList[Cursor]] = {
-  // Group cursors into lines first
+  // Group the stream into lines
   val lines = groupIntoLines(stream)
 
-  // Group lines into paragraphs (of cursors)
-  var paragraphs: List[LazyList[Cursor]]       = Nil
-  var currentParagraph: List[LazyList[Cursor]] = Nil
-
-  for (line <- lines) {
-    // Debug the line content
-    // println(s"Line: '${line.map(_.char).mkString}'")
-    // println(s"Is blank: ${isBlankLine(line)}")
-
-    if (isBlankLine(line) && currentParagraph.nonEmpty) {
-      // End of paragraph - concatenate the cursor lines
-      val paragraphCursors = currentParagraph.reverse.foldLeft(LazyList.empty[Cursor])(_ ++ _)
-      paragraphs = paragraphCursors :: paragraphs
-      currentParagraph = Nil
-    } else if (!isBlankLine(line)) {
-      // Continue paragraph
-      currentParagraph = line :: currentParagraph
-    }
-    // Skip blank lines that don't end paragraphs
-  }
-
-  // Add final paragraph if there is one
-  if (currentParagraph.nonEmpty) {
-    val paragraphCursors = currentParagraph.reverse.foldLeft(LazyList.empty[Cursor])(_ ++ _)
-    paragraphs = paragraphCursors :: paragraphs
-  }
-
-  paragraphs.reverse
+  // Process lines to detect block structures
+  processLines(lines)
 }
 
+// Interface for block parsers
+trait BlockParser {
+  // Check if this parser can handle the given line
+  def canStart(line: LazyList[Cursor]): Boolean
+
+  // Parse a block starting with the given line
+  // Returns the parsed block and the number of lines consumed
+  def parse(lines: List[LazyList[Cursor]]): (Block, Int)
+}
+
+// Process lines to build blocks
+private def processLines(lines: List[LazyList[Cursor]]): List[Block] = {
+  var blocks: List[Block] = Nil
+  var remainingLines      = lines
+
+  // The list of block parsers in priority order
+  val blockParsers: List[BlockParser] = List(
+    ParagraphBlockParser, // We'll add more parsers here later
+  )
+
+  // Process lines until none remain
+  while (remainingLines.nonEmpty) {
+    // Find a parser for the current line
+    blockParsers.find(_.canStart(remainingLines.head)) match {
+      case Some(parser) =>
+        // Parse the block and update remaining lines
+        val (block, linesConsumed) = parser.parse(remainingLines)
+        blocks = blocks :+ block
+        remainingLines = remainingLines.drop(linesConsumed)
+
+      case None =>
+        // Skip unrecognized lines (shouldn't happen in practice)
+        remainingLines = remainingLines.tail
+    }
+  }
+
+  blocks
+}
+
+// A parser for paragraph blocks - our first concrete implementation
+object ParagraphBlockParser extends BlockParser {
+  def canStart(line: LazyList[Cursor]): Boolean = {
+    // A paragraph can start with any non-blank line
+    !isBlankLine(line)
+  }
+
+  def parse(lines: List[LazyList[Cursor]]): (Block, Int) = {
+    // Find the first blank line
+    val paragraphLines = lines.takeWhile(line => !isBlankLine(line))
+
+    // The actual number of lines consumed is the paragraph plus the blank line
+    val linesConsumed = if (paragraphLines.length < lines.length) {
+      paragraphLines.length + 1 // Include the blank line that terminated the paragraph
+    } else {
+      paragraphLines.length // The paragraph runs to the end
+    }
+
+    // Convert the lines to a text block
+    val text      = paragraphLines.flatten.map(_.char).mkString
+    val paragraph = Paragraph(List(Text(text)))
+
+    (paragraph, linesConsumed)
+  }
+}
+
+// Function to check if a line is blank
 private def isBlankLine(line: LazyList[Cursor]): Boolean = {
   // A blank line contains only whitespace or is empty (excluding newline)
   val contentChars = line.filter(_.char != '\n')
   contentChars.isEmpty || contentChars.forall(c => c.char == ' ' || c.char == '\t')
 }
 
+// Group cursor stream into lines
 private def groupIntoLines(stream: LazyList[Cursor]): List[LazyList[Cursor]] = {
   var lines: List[LazyList[Cursor]] = Nil
   var currentLine: List[Cursor]     = Nil
