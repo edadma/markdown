259dfce6075639225f81e5d1a8671a879e008dde
Edward A Maxedon <edadma@gmail.com>
2025-04-21 17:36:03 -0400

    inline nodes

--- shared/src/main/scala/io/github/edadma/markdown/Node.scala
+++ shared/src/main/scala/io/github/edadma/markdown/Node.scala
@@ -10,7 +10,14 @@ case class Code(content: String, infoString: Option[String] = None) extends Bloc
 case class BlockQuote(children: List[Block])                        extends Block
 case class ThematicBreak()                                          extends Block
 
-sealed trait Inline              extends Node
-case class Text(content: String) extends Inline
-case class SoftLineBreak()       extends Inline
-case class HardLineBreak()       extends Inline
+sealed trait Inline                                                                 extends Node
+case class Text(content: String)                                                    extends Inline
+case class SoftLineBreak()                                                          extends Inline
+case class HardLineBreak()                                                          extends Inline
+case class CodeSpan(content: String)                                                extends Inline
+case class Emphasis(inlines: List[Inline])                                          extends Inline
+case class Strong(inlines: List[Inline])                                            extends Inline
+case class Link(destination: String, title: Option[String], inlines: List[Inline])  extends Inline
+case class Image(destination: String, title: Option[String], inlines: List[Inline]) extends Inline
+case class AutoLink(destination: String, text: String)                              extends Inline
+case class RawHTML(content: String)                                                 extends Inline
